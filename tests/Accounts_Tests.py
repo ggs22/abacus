a3d7a0fa34d92936d176dcf7bcfe1d7afb260044
Gabriel Gibeau Sanchez <ggs8922@gmail.com>
2021-12-08 18:45:36 -0500

    testing mc balance prediction with added pays

--- tests/Accounts_Tests.py
+++ tests/Accounts_Tests.py
@@ -3,19 +3,17 @@ from Accounts import *
 
 
 #%%
-
 # for year in [2019, 2020, 2021]:
 #     accounts.barplot(year=year, show=True)
-    # account.get_summed_data(year=year)
-    # account.barplot(year=year, average=False, show=True)
-    # account.barplot(year=year, average=True, show=True)
 
 
-paypal.update_from_raw_files()
+#%%
 # for acc in accounts:
 #     print(acc.metadata.name.name)
 #     print(acc.most_recent_date)
 
+#%%
+tst = desjardins_mc.get_predicted_balance()
 
 # desjardins_op.apply_description_filter(pattern=r"[Pp][Aa][Yy][Pp][Aa][Ll]", regex=True)
 # desjardins_op[0]
